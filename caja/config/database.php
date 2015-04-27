2aea8d5d8a610f8970cb7ae6473d2943c54ef2f7
Carlos Visbal <cvisbal0724@gmail.com>
2015-04-27 09:10:25 -0500

    Cambio DB
    Cambio DB

--- caja/config/database.php
+++ caja/config/database.php
@@ -77,10 +77,10 @@ return [
 
 		'sqlsrv' => [
 			'driver'   => 'sqlsrv',
-			'host'     => 'CARLOSVISBALR,2014\SQLEXPRESS2014',
-			'database' => 'sinin',
-			'username' => 'sa',
-			'password' => '12345',
+			'host'     => env('DB_HOST', 'localhost'),
+			'database' => env('DB_DATABASE', 'forge'),
+			'username' => env('DB_USERNAME', 'forge'),
+			'password' => env('DB_PASSWORD', ''),
 			'prefix'   => '',
 		],
 
